6fa0ea1e11cd18bea6e5802958ec248b005a8670
Aleksandr <sky7way@yandex.ru>
2023-05-27 15:45:17 +0300

    filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+export const App = () => {
 
     const [money, setMoney] = useState([
         {banknots: 'Dollars', value: 100, number: ' a1234567890'},
@@ -14,23 +14,42 @@ function App() {
         {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
     ])
 
-    return (
-        <ul>
-            {money.map((objectFromMoneyArray: {
-                banknots: string;
-                value: number;
-                number: string;
-            }, index: number) => {
-                return (
-                    <li key={index}>
-                        <span>{objectFromMoneyArray.banknots}</span>
-                        <span>{objectFromMoneyArray.value}</span>
-                        <span>{objectFromMoneyArray.number}</span>
-                    </li>
-                );
-            })}
-        </ul>
-    );
-}
+    const [filter, setFilter]=useState('All Money')
 
-export default App;
+    let currentMoney = money;
+    if (filter === 'rubles') {
+        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'RUBLS');
+    }
+    if (filter === 'dollars') {
+        currentMoney = money.filter(filteredMoney => filteredMoney.banknots === 'Dollars');
+    }
+        const onClickFilterHandler = (nameButton: string) => {
+        setFilter((nameButton))
+        };
+
+
+        return (
+            <>
+                <ul>
+                    {currentMoney.map((objectFromMoneyArray: {
+                        banknots: string;
+                        value: number;
+                        number: string;
+                    }, index: number) => {
+                        return (
+                            <li key={index}>
+                                <span> {objectFromMoneyArray.banknots}</span>
+                                <span> {objectFromMoneyArray.value}</span>
+                                <span> {objectFromMoneyArray.number}</span>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <div style={{marginLeft: '30px'}}>
+                    <button onClick={() => onClickFilterHandler('allMoney')}>All Money</button>
+                    <button onClick={() => onClickFilterHandler('rubles')}>Rubles</button>
+                    <button onClick={() => onClickFilterHandler('dollars')}>Dollars</button>
+                </div>
+            </>
+        );
+    }
