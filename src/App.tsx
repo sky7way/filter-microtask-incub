84a2074488f6a4d79d1f690793bf3f95aa6a7615
Aleksandr <sky7way@yandex.ru>
2023-05-27 11:23:59 +0300

    add list of banknots

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+
+    return (
+        <ul>
+            {money.map((objectFromMoneyArray: {
+                banknots: string;
+                value: number;
+                number: string;
+            }, index: number) => {
+                return (
+                    <li key={index}>
+                        <span>{objectFromMoneyArray.banknots}</span>
+                        <span>{objectFromMoneyArray.value}</span>
+                        <span>{objectFromMoneyArray.number}</span>
+                    </li>
+                );
+            })}
+        </ul>
+    );
 }
 
 export default App;
